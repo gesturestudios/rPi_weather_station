598da2a6123ae0f222b4565aba7ad3b7b20054db
gesturestudios <gesturestudios@gmail.com>
2017-10-22 21:53:10 -0700

    Update clean_PGE_data.R

--- clean_PGE_data.R
+++ clean_PGE_data.R
@@ -10,6 +10,6 @@ data$usage_date = as.Date(data$str_date,"%m/%d/%Y")
 exp_data = data[c(ncol(data),2:(ncol(data)-1))]
 
 # now connect to MySQL db and insert the data:
-mydb = dbConnect(MySQL(), user='arduinouser', password='arduino', dbname='arduino_data', host='127.0.0.1')
+mydb = dbConnect(MySQL(), user='[user]', password='[pwd]', dbname='arduino_data', host='127.0.0.1')
 #dbListTables(mydb)
 dbWriteTable(mydb,"PGE_usage",exp_data,append=TRUE, row.names=FALSE)
